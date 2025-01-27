9f260761646862c49f716e2aedcc8ad6eabebe6c
mequis <mequis456@gmail.com>
2025-01-27 17:12:25 +0100

    added auto github push
    -testing data changing on other leagues

--- pythonProject1/scrape.py
+++ pythonProject1/scrape.py
@@ -1,5 +1,7 @@
 import json
 import time
+import os
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -13,10 +15,48 @@ chrome_options.add_argument("--no-sandbox")
 
 # chromedriver ścieżka
 service = Service("Y:\\chromedriver.exe")
+# ścieżka teams_data.json (pliku z danymi)
+file_path = "Y:\\xampp\\htdocs\\ucl\\teams_data.json"
 
+# sprawdzanie czasu modyfikacji pliku
+def get_file_modification_time(file_path):
+    return os.path.getmtime(file_path)
+
+#sprawdzanie czasu ostatniego commitu
+def get_last_commit_time():
+    result = subprocess.run(
+        ['git', 'log', '-1', '--format=%ct'], capture_output=True, text=True
+    )
+    if result.returncode == 0:
+        return int(result.stdout.strip())
+    else:
+        raise Exception("Błąd podczas pobierania ostatniego commitu.")
+
+# wymuszanie commitu
+def force_commit_if_modified():
+    try:
+        # Pobierz czas modyfikacji pliku i czas ostatniego commitu
+        file_mod_time = get_file_modification_time(file_path)
+        last_commit_time = get_last_commit_time()
+
+        # Porównaj czasy
+        if file_mod_time > last_commit_time:
+            print("Plik został zmodyfikowany po ostatnim commicie. Wykonuję commit...")
+            subprocess.run(['git', 'add', file_path])  # Dodaj zmiany do staging area
+            subprocess.run(['git', 'commit', '-m', 'Automatyczny commit po zmianie pliku'])
+            print("Wykonuję push do zdalnego repozytorium...")
+            subprocess.run(['git', 'push', 'origin', 'main'])  # Zakładając, że główną gałęzią jest 'main'
+        else:
+            print("Brak zmian w pliku po ostatnim commicie.")
+    except Exception as e:
+        print(f"Błąd: {e}")
+
+
+
+# datascraping
 def scrape_data():
     driver = webdriver.Chrome(service=service, options=chrome_options)
-    url = "https://www.livescore.com/en/football/champions-league/league-stage/table/"
+    url = "https://www.livescore.com/en/football/italy/serie-a/table/"
     driver.get(url)
     driver.implicitly_wait(20)
 
@@ -41,7 +81,6 @@ def scrape_data():
         driver.quit()
 
     # zapis
-    file_path = "Y:\\xampp\\htdocs\\ucl\\teams_data.json"
     data_sorted = sorted(data, key=lambda x: x['team_name'])
     with open(file_path, "w") as file:
         json.dump(data_sorted, file)
@@ -53,5 +92,6 @@ def scrape_data():
 while True:
     print("Rozpoczynam scrapowanie danych...")
     scrape_data()
+    force_commit_if_modified()
     print("Oczekiwanie 60 sekund...")
     time.sleep(60)
